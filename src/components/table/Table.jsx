cb48294829fefd42b6a783b1c86d7317176f8cd1
jyoti <jthakur777.jt@gmail.com>
2024-05-11 23:24:45 +0530

    added dynamic table component

---
+++ src/components/table/Table.jsx
@@ -0,0 +1,101 @@
+import { useState } from "react";
+import "../table/table.css";
+const Table = () => {
+  const [tableData, setTableData] = useState([]);
+  const [name, setName] = useState("");
+  const [standard, setStandard] = useState("");
+  const [rollNo, setRollNo] = useState("");
+
+  const [isEditing, setIsEditing] = useState(false);
+  const [idForEdit, setIdForEdit] = useState("null");
+
+  const handleNameOnEdit = (ele) => {
+    setIdForEdit(ele.id);
+    setIsEditing(true);
+    setName(ele.name);
+    setStandard(ele.standard);
+    setRollNo(ele.rollNo);
+  };
+
+  const handleOnCancel = () => {
+    setIsEditing(false);
+  };
+  const handleDelete = (id) => {
+    const filteredList = tableData.filter((ele) => ele.id !== id);
+    setTableData(filteredList);
+  };
+  const handleSave = (ele) => {
+    ele.name = name;
+    ele.standard = standard;
+    ele.rollNo = rollNo;
+    setIsEditing(false);
+  };
+  const addNewRow =() =>{
+    setTableData([...tableData,{name:"",standard:"",rollNo:"",id:Date.now()}]);
+  }
+
+  return (
+    <div className="table-div">
+      <button className="add-row-btn" onClick={addNewRow}>Add Row</button>
+      <table>
+        <tr>
+          <th>Name</th>
+          <th>Standard</th>
+          <th>Roll No.</th>
+          <th>Actions</th>
+        </tr>
+        {tableData.map((ele) => {
+          return (
+            <tr key={ele.id}>
+              {isEditing && ele.id === idForEdit ? (
+                <td>
+                  <input
+                    type="text"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                  />
+                </td>
+              ) : (
+                <td>{ele.name}</td>
+              )}
+              {isEditing && ele.id === idForEdit ? (
+                <td>
+                  <input
+                    type="text"
+                    value={standard}
+                    onChange={(e) => setStandard(e.target.value)}
+                  />
+                </td>
+              ) : (
+                <td>{ele.standard}</td>
+              )}
+              {isEditing && ele.id === idForEdit ? (
+                <td>
+                  <input
+                    type="text"
+                    value={rollNo}
+                    onChange={(e) => setRollNo(e.target.value)}
+                  />
+                </td>
+              ) : (
+                <td>{ele.rollNo}</td>
+              )}
+              {isEditing && ele.id === idForEdit ? (
+                <td>
+                  <button className="primary-btn" onClick={() => handleSave(ele)}>Save</button>
+                  <button className="secondary-btn" onClick={handleOnCancel}>Cancel</button>
+                </td>
+              ) : (
+                <td>
+                  <button className="primary-btn" onClick={() => handleNameOnEdit(ele)}>Edit</button>
+                  <button className="delete-btn" onClick={() => handleDelete(ele.id)}>Delete</button>
+                </td>
+              )}
+            </tr>
+          );
+        })}
+      </table>
+    </div>
+  );
+};
+export default Table;
